e2808d52832cbacf00b0c162a930135e3f3d775f
Jerry Lee <372154251@qq.com>
2016-03-20 11:51:43 +0800

    创建Demo

---
+++ JYPhotoBrowserDemo/JYPhotoBrowserDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JYPhotoBrowserDemo
+//
+//  Created by 李少帅 on 16/3/20.
+//  Copyright © 2016年 李少帅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
